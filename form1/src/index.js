b846bd8839e5fa61a418b371ff124d5b221412a8
PradeepMistry <chauhanpradeep341@gmail.com>
2020-03-24 19:08:34 +0530

    add code

--- form1/src/index.js
+++ form1/src/index.js
@@ -5,9 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import Demo from './Demo';
 
-ReactDOM.render(
-    <Demo/>,document.getElementById('root')
-);
+ReactDOM.render(<Demo/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
